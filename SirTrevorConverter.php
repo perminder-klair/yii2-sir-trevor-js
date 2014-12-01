34c4e9df7989e64dc51f8c1b8e7e4a147988f716
Parminder Klair <perminder.klair@gmail.com>
2014-12-01 15:36:55 +0000

    convertor for sirtrevor

--- SirTrevorConverter.php
+++ SirTrevorConverter.php
@@ -37,8 +37,8 @@ use \Michelf\MarkdownExtra as Markdown;
  * @package kato\sirtrevorjs
  * @author <pascalbrewing@gmail.com>
  */
-
-class SirTrevorConverter {
+class SirTrevorConverter
+{
     /**
      * Code JS needed by elements
      *
@@ -46,6 +46,8 @@ class SirTrevorConverter {
      * @var array
      */
     protected $codejs = null;
+
+    public $textImageRenderClass = 'frontend\components\TextImageRender';
 
     /**
      * Converts the outputted json from Sir Trevor to html
@@ -57,7 +59,7 @@ class SirTrevorConverter {
     {
         // convert the json to an associative array
         $input = json_decode($json, true);
-        $html  = null;
+        $html = null;
 
         if (!empty($input) && is_array($input)) {
             // loop trough the data blocks
@@ -70,12 +72,14 @@ class SirTrevorConverter {
 
                 // check if we have a converter for this type
                 $converter = $block['type'] . 'ToHtml';
-                if (is_callable(array($this, $converter))) {
+                if (is_callable(array($this, $converter))) {dump($block['data']);exit;
                     // call the function and add the data as parameters
                     $html .= call_user_func_array(
                         array($this, $converter),
                         $block['data']
                     );
+                } elseif ($block['type'] == "textimage") {
+                    $html .= $this->renderTextImage($block['data']);
                 } elseif ($block['type'] == "tweet") {
                     // special twitter
                     $html .= $this->twitterToHtml($block['data']);
@@ -165,12 +169,12 @@ class SirTrevorConverter {
      */
     public function imageToHtml($file, $caption = '')
     {
-        $url = (isset($file['url']))?$file['url']:'';
+        $url = (isset($file['url'])) ? $file['url'] : '';
 
         $_return = '<figure class="st-image"><img src="' . $url . '" alt="" />';
 
         if ($caption != null) {
-            $_return .= '<figcaption>'.$caption.'</figcaption>';
+            $_return .= '<figcaption>' . $caption . '</figcaption>';
         }
 
         $_return .= '</figure>';
@@ -196,21 +200,21 @@ class SirTrevorConverter {
              * Youtube
              */
             case "youtube":
-                $html = '<iframe width="580" height="320" src="//www.youtube.com/embed/'.$remote_id.'" frameborder="0" allowfullscreen></iframe>';
+                $html = '<iframe width="580" height="320" src="//www.youtube.com/embed/' . $remote_id . '" frameborder="0" allowfullscreen></iframe>';
                 break;
 
             /**
              * Vimeo
              */
             case "vimeo":
-                $html = '<iframe src="//player.vimeo.com/video/'.$remote_id.'?title=0&amp;byline=0" width="580" height="320" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+                $html = '<iframe src="//player.vimeo.com/video/' . $remote_id . '?title=0&amp;byline=0" width="580" height="320" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
                 break;
 
             /**
              * Dailymotion
              */
             case "dailymotion":
-                $html = '<iframe frameborder="0" width="580" height="320" src="//www.dailymotion.com/embed/video/'.$remote_id.'"></iframe>';
+                $html = '<iframe frameborder="0" width="580" height="320" src="//www.dailymotion.com/embed/video/' . $remote_id . '"></iframe>';
                 break;
 
             /**
@@ -219,105 +223,105 @@ class SirTrevorConverter {
             case "vine":
                 $this->codejs['vine'] = '<script async src="http://platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>';
 
-                $html = '<iframe class="vine-embed" src="//vine.co/v/'.$remote_id.'/embed/simple" width="580" height="320" frameborder="0"></iframe>';
+                $html = '<iframe class="vine-embed" src="//vine.co/v/' . $remote_id . '/embed/simple" width="580" height="320" frameborder="0"></iframe>';
                 break;
 
             /**
              * Metacafe
              */
             case "metacafe":
-                $html = '<iframe src="http://www.metacafe.com/embed/'.$remote_id.'/" width="540" height="304" allowFullScreen frameborder=0></iframe>';
+                $html = '<iframe src="http://www.metacafe.com/embed/' . $remote_id . '/" width="540" height="304" allowFullScreen frameborder=0></iframe>';
                 break;
 
             /**
              * Yahoo video
              */
             case "yahoo":
-                $html = '<iframe width="640" height="360" scrolling="no" frameborder="0" src="http://screen.yahoo.com/embed/'.$remote_id.'.html" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" allowtransparency="true"></iframe>';
+                $html = '<iframe width="640" height="360" scrolling="no" frameborder="0" src="http://screen.yahoo.com/embed/' . $remote_id . '.html" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" allowtransparency="true"></iframe>';
                 break;
 
             /**
              * UStream Live
              */
             case "ustream":
-                $html = '<iframe width="640" height="392" src="http://www.ustream.tv/embed/'.$remote_id.'?v=3&amp;wmode=direct" scrolling="no" frameborder="0" style="border: 0px none transparent;"></iframe>';
+                $html = '<iframe width="640" height="392" src="http://www.ustream.tv/embed/' . $remote_id . '?v=3&amp;wmode=direct" scrolling="no" frameborder="0" style="border: 0px none transparent;"></iframe>';
                 break;
 
             /**
              * UStream Recorded
              */
             case "ustreamrecord":
-                $html = '<iframe width="640" height="392" src="http://www.ustream.tv/embed/recorded/'.$remote_id.'?v=3&amp;wmode=direct" scrolling="no" frameborder="0" style="border: 0px none transparent;"></iframe>';
+                $html = '<iframe width="640" height="392" src="http://www.ustream.tv/embed/recorded/' . $remote_id . '?v=3&amp;wmode=direct" scrolling="no" frameborder="0" style="border: 0px none transparent;"></iframe>';
                 break;
 
             /**
              * Veoh
              */
             case "veoh":
-                $html = '<object width="640" height="532" id="veohFlashPlayer" name="veohFlashPlayer"><param name="movie" value="http://www.veoh.com/swf/webplayer/WebPlayer.swf?version=AFrontend.5.7.0.1444&amp;permalinkId='.$remote_id.'&amp;player=videodetailsembedded&amp;videoAutoPlay=0&amp;id=anonymous"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.veoh.com/swf/webplayer/WebPlayer.swf?version=AFrontend.5.7.0.1444&amp;permalinkId='.$remote_id.'&amp;player=videodetailsembedded&amp;videoAutoPlay=0&amp;id=anonymous" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="640" height="532" id="veohFlashPlayerEmbed" name="veohFlashPlayerEmbed"></embed></object>';
+                $html = '<object width="640" height="532" id="veohFlashPlayer" name="veohFlashPlayer"><param name="movie" value="http://www.veoh.com/swf/webplayer/WebPlayer.swf?version=AFrontend.5.7.0.1444&amp;permalinkId=' . $remote_id . '&amp;player=videodetailsembedded&amp;videoAutoPlay=0&amp;id=anonymous"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.veoh.com/swf/webplayer/WebPlayer.swf?version=AFrontend.5.7.0.1444&amp;permalinkId=' . $remote_id . '&amp;player=videodetailsembedded&amp;videoAutoPlay=0&amp;id=anonymous" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="640" height="532" id="veohFlashPlayerEmbed" name="veohFlashPlayerEmbed"></embed></object>';
                 break;
 
             /**
              * Vevo
              */
             case "vevo":
-                $html = '<iframe width="575" height="324" src="http://cache.vevo.com/m/html/embed.html?video='.$remote_id.'" frameborder="0" allowfullscreen></iframe>';
+                $html = '<iframe width="575" height="324" src="http://cache.vevo.com/m/html/embed.html?video=' . $remote_id . '" frameborder="0" allowfullscreen></iframe>';
                 break;
 
             /**
              * AOL
              */
             case "aol":
-                $html = '<script type="text/javascript" src="http://pshared.5min.com/Scripts/PlayerSeed.js?sid=281&amp;width=560&amp;height=345&amp;playList='.$remote_id.'"></script>';
+                $html = '<script type="text/javascript" src="http://pshared.5min.com/Scripts/PlayerSeed.js?sid=281&amp;width=560&amp;height=345&amp;playList=' . $remote_id . '"></script>';
                 break;
 
             /**
              * Metatube
              */
             case "metatube":
-                $html = '<iframe width="640" height="480" src="http://www.metatube.com/en/videos/'.$remote_id.'/embed/" frameborder="0" allowfullscreen></iframe>';
+                $html = '<iframe width="640" height="480" src="http://www.metatube.com/en/videos/' . $remote_id . '/embed/" frameborder="0" allowfullscreen></iframe>';
                 break;
 
             /**
              * Wat.tv
              */
             case "wat":
-                $html = '<iframe src="http://www.wat.tv/embedframe/'.$remote_id.'\" frameborder="0" style="width: 640px; height: 360px;"></iframe>';
+                $html = '<iframe src="http://www.wat.tv/embedframe/' . $remote_id . '\" frameborder="0" style="width: 640px; height: 360px;"></iframe>';
                 break;
 
             /**
              * Daily Mail UK
              */
             case "dailymailuk":
-                $html = '<iframe frameborder="0" width="698" height="503" scrolling="no" id="molvideoplayer" title="MailOnline Embed Player" src="http://www.dailymail.co.uk/embed/video/'.$remote_id.'.html" ></iframe>';
+                $html = '<iframe frameborder="0" width="698" height="503" scrolling="no" id="molvideoplayer" title="MailOnline Embed Player" src="http://www.dailymail.co.uk/embed/video/' . $remote_id . '.html" ></iframe>';
                 break;
 
             /**
              * Canal Plus
              */
             case "cplus":
-                $html = '<iframe width="640" height="360" frameborder="0" scrolling="no" src="http://player.canalplus.fr/embed/?param=cplus&amp;vid='.$remote_id.'"></iframe>';
+                $html = '<iframe width="640" height="360" frameborder="0" scrolling="no" src="http://player.canalplus.fr/embed/?param=cplus&amp;vid=' . $remote_id . '"></iframe>';
                 break;
 
             /**
              * France Television
              */
             case "francetv":
-                $html = '<iframe frameborder="0" width="640" height="360" src="http://api.dmcloud.net/player/embed/'.$remote_id.'?exported=1"></iframe>';
+                $html = '<iframe frameborder="0" width="640" height="360" src="http://api.dmcloud.net/player/embed/' . $remote_id . '?exported=1"></iframe>';
                 break;
 
             /**
              * Zoomin.tv
              */
             case "zoomin":
-                $html = '<iframe src="http://blackbird.zoomin.tv/players/.pla?pid=corporatefr&amp;id='.$remote_id.'&amp;w=655&amp;h=433" style="width:655px; height:433px; border:none; overflow:hidden;" frameborder="0" scrolling="no" allowtransparency="yes"></iframe>';
+                $html = '<iframe src="http://blackbird.zoomin.tv/players/.pla?pid=corporatefr&amp;id=' . $remote_id . '&amp;w=655&amp;h=433" style="width:655px; height:433px; border:none; overflow:hidden;" frameborder="0" scrolling="no" allowtransparency="yes"></iframe>';
                 break;
 
             /**
              * Global News
              */
             case "globalnews":
-                $html = '<iframe src="http://globalnews.ca/video/embed/'.$remote_id.'/" width="670" height="437" frameborder="0" allowfullscreen></iframe>';
+                $html = '<iframe src="http://globalnews.ca/video/embed/' . $remote_id . '/" width="670" height="437" frameborder="0" allowfullscreen></iframe>';
                 break;
         }
 
@@ -325,21 +329,32 @@ class SirTrevorConverter {
          * Caption
          */
         if ($html != null && $caption != null) {
-            $html .= '<figcaption>'.$caption.'</figcaption>';
+            $html .= '<figcaption>' . $caption . '</figcaption>';
         }
 
         if ($html != null) {
-            return '<figure class="st-movie">'.$html.'</figure>';
+            return '<figure class="st-movie">' . $html . '</figure>';
         }
 
         return null;
     }
 
     /**
+     * Calls Run method in defined class in textImageRenderClass
+     * @param $data
+     * @return mixed
+     */
+    public function renderTextImage($data)
+    {
+        return call_user_func(array($this->textImageRenderClass, 'run'), $data);
+    }
+
+    /**
      * set a single column html
      * @param $column
      */
-    private function _columns($column){
+    private function _columns($column)
+    {
 
     }
 
